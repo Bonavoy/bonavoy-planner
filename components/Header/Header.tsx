0c06042692977d50a8725012dbd86836e2e7dbf4
Shane Goonasekera <theshanukg@gmail.com>
2023-02-01 02:08:56 -0700

    removed comments

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,40 +6,6 @@ type HeaderProps = {
   mode: 'planner' | 'flights';
 };
 
-// {/* <section className="flex h-full items-center">
-// <div className="flex h-full items-center bg-[#2F3C7E] px-4 text-white">
-//   {/* <span className="text-2xl font-bold">B</span> */}
-//   {/* <Link href="/trips" className="fa-solid fa-chevron-left" /> */}
-//   <span className="flex cursor-pointer items-center gap-2 text-2xl font-black">
-//     {mode}
-//     <i className="fa-solid fa-caret-down" />
-//   </span>
-//   {/* <ul className="flex items-center gap-1">
-//     {navs.map((nav) => (
-//       <li key={nav.name}>
-//         <Link
-//           href={`/trips/${tripId}/${nav.name}`}
-//           className={
-//             'flex items-center justify-center gap-1 rounded-full border border-grayPrimary px-3'
-//           }
-//         >
-//           {nav.icon}
-//           {nav.name}
-//         </Link>
-//       </li>
-//     ))}
-//   </ul> */}
-// </div>
-// <div className="flex h-full flex-grow justify-between px-4">
-//   <input
-//     type="text"
-//     autoComplete="off"
-//     className="bg-opacity-0 text-xl focus:outline-none"
-//     placeholder="Name your adventure"
-//   />
-// </div>
-// </section> */}
-
 export default function Header({ tripId, mode }: HeaderProps) {
   const navs = [
     {
--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
@@ -13,21 +13,8 @@ export default function Mapbox() {
       accessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
       container: mapContainer.current as HTMLElement,
       style: 'mapbox://styles/mapbox/streets-v12',
-      //   dragPan: false,
-      //   doubleClickZoom: false,
-      //   scrollZoom: false,
-      //   keyboard: false,
     });
   });
 
-  //   useEffect(() => {
-  //     if (!map.current) return; // wait for map to initialize
-  //     map.current.on('move', () => {
-  //       setLng(map.current.getCenter().lng.toFixed(4));
-  //       setLat(map.current.getCenter().lat.toFixed(4));
-  //       setZoom(map.current.getZoom().toFixed(2));
-  //     });
-  //   });
-
-  return <div ref={mapContainer} className="h-full w-full map-container" />;
+  return <div ref={mapContainer} className="map-container h-full w-full" />;
 }
