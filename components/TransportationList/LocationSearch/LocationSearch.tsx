58ab402f5595ac922fb0b55bf43cdd9188cc8168
Neilzon <neilcviloria@gmail.com>
2023-05-01 11:46:39 -0600

    border clolor

--- components/TransportationList/LocationSearch/LocationSearch.tsx
+++ components/TransportationList/LocationSearch/LocationSearch.tsx
@@ -71,8 +71,9 @@ const LocationSearch = ({
   return (
     <div
       className={clsx(
-        'relative col-start-2 box-content flex flex-1 items-center gap-2 rounded-md border-2 border-white px-2 duration-150 hover:bg-surface',
+        'relative col-start-2 box-content flex flex-1 items-center gap-2 rounded-md border-2 px-2 duration-150 hover:bg-surface',
         { 'border-primary': activeElements.has(elementId) },
+        { 'border-transparent': !activeElements.has(elementId) },
       )}
     >
       <i
