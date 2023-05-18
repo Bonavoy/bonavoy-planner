13102b4b500c4c30845eb6d5cc77b98872ad3154
Shane Goonasekera <theshanukg@gmail.com>
2023-05-17 19:44:06 -0600

    stuff on day plans

--- components/Place/Place.tsx
+++ components/Place/Place.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from '@apollo/client';
+import { eachDayOfInterval } from 'date-fns';
 
 //queries
 import { GET_PLACE } from '~/graphql/queries/place';
 
+//utils
+import { formatDate } from '~/utils/date';
 interface PlaceProps {
   placeId: string;
 }
@@ -12,11 +15,45 @@ export default function Place({ placeId }: PlaceProps) {
     variables: { placeId },
   });
 
+  //TODO: better loader
+  if (placeDetailsQuery.loading) return <article>loading..</article>;
+
   return (
-    <section className="overflow-y-auto">
-      <div className="h-full overflow-y-auto px-16 py-8">
+    <article>
+      <h1 className="text-3xl font-bold">
         {placeDetailsQuery.data?.place.placeName}
+      </h1>
+      <p>
+        {formatDate(new Date(placeDetailsQuery.data?.place.startDate), {
+          month: 'short',
+          day: 'numeric',
+        })}{' '}
+        -{' '}
+        {formatDate(new Date(placeDetailsQuery.data?.place.endDate), {
+          month: 'short',
+          day: 'numeric',
+        })}
+      </p>
+
+      <div className="my-4 flex gap-8 rounded-xl bg-grayPrimary/5">
+        {eachDayOfInterval({
+          start: new Date(placeDetailsQuery.data?.place.startDate),
+          end: new Date(placeDetailsQuery.data?.place.endDate),
+        }).map((date, index) => (
+          <h2
+            className="flex flex-col p-2 text-lg font-semibold leading-tight"
+            key={date.getTime()}
+          >
+            Day {index + 1}
+            <span className="text-xs font-normal">
+              {formatDate(date, {
+                month: 'short',
+                day: 'numeric',
+              })}
+            </span>
+          </h2>
+        ))}
       </div>
-    </section>
+    </article>
   );
 }
--- pages/trips/[tripId]/planner/index.tsx
+++ pages/trips/[tripId]/planner/index.tsx
@@ -41,71 +41,73 @@ export default function Plan({ tripId, placeId }: PlanProps) {
         details={plannerDetailsQuery.data?.plannerDetails!}
       >
         <div className="grid flex-grow grid-cols-2 bg-background">
-          {placeId ? (
-            <PlaceView placeId={placeId} />
-          ) : (
-            <section className="overflow-y-auto">
-              <div className="h-full overflow-y-auto px-16 py-8">
-                <div className="relative mb-4 flex h-2/6 items-center justify-center rounded-xl bg-surface">
-                  {plannerDetailsQuery.data?.plannerDetails.banner ? (
-                    <Image
-                      className="rounded-xl object-cover object-center"
-                      fill={true}
-                      alt="banner"
-                      loader={() =>
-                        plannerDetailsQuery.data?.plannerDetails.banner!
-                      }
-                      src="banner"
-                      quality={100}
-                      priority={true}
-                    />
-                  ) : (
-                    <p className="text-grayPrimary">+ Add a photo</p>
-                  )}
-                </div>
-                <div className="mb-10 flex items-center justify-between rounded-xl font-sans text-white">
-                  <div className="grid grid-cols-2 gap-3">
-                    <button className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-2 font-sans">
-                      <i className="fa-solid fa-calendar" />
-                      {formatDate(
-                        new Date(
-                          plannerDetailsQuery.data?.plannerDetails.startDate,
-                        ),
-                        { month: 'short', day: 'numeric' },
-                      )}{' '}
-                      -{' '}
-                      {formatDate(
-                        new Date(
-                          plannerDetailsQuery.data?.plannerDetails.endDate,
-                        ),
-                        { month: 'short', day: 'numeric' },
-                      )}
-                    </button>
-                    <div className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-2">
-                      <i className="fa-solid fa-location-dot" />
-                      {
-                        plannerDetailsQuery.data?.plannerDetails.places.length
-                      }{' '}
-                      Stops
-                    </div>
+          <section className="overflow-y-auto">
+            <div className="h-full overflow-y-auto px-16 py-8">
+              {placeId ? (
+                <PlaceView placeId={placeId} />
+              ) : (
+                <article>
+                  <div className="relative mb-4 flex h-2/6 items-center justify-center rounded-xl bg-surface">
+                    {plannerDetailsQuery.data?.plannerDetails.banner ? (
+                      <Image
+                        className="rounded-xl object-cover object-center"
+                        fill={true}
+                        alt="banner"
+                        loader={() =>
+                          plannerDetailsQuery.data?.plannerDetails.banner!
+                        }
+                        src="banner"
+                        quality={100}
+                        priority={true}
+                      />
+                    ) : (
+                      <p className="text-grayPrimary">+ Add a photo</p>
+                    )}
                   </div>
-                  <div className="flex items-center justify-center gap-4 text-primary">
-                    {/* <button>
+                  <div className="mb-10 flex items-center justify-between rounded-xl font-sans text-white">
+                    <div className="grid grid-cols-2 gap-3">
+                      <button className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-2 font-sans">
+                        <i className="fa-solid fa-calendar" />
+                        {formatDate(
+                          new Date(
+                            plannerDetailsQuery.data?.plannerDetails.startDate,
+                          ),
+                          { month: 'short', day: 'numeric' },
+                        )}{' '}
+                        -{' '}
+                        {formatDate(
+                          new Date(
+                            plannerDetailsQuery.data?.plannerDetails.endDate,
+                          ),
+                          { month: 'short', day: 'numeric' },
+                        )}
+                      </button>
+                      <div className="flex items-center justify-center gap-2 rounded-xl bg-primary px-4 py-2">
+                        <i className="fa-solid fa-location-dot" />
+                        {
+                          plannerDetailsQuery.data?.plannerDetails.places.length
+                        }{' '}
+                        Stops
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-center gap-4 text-primary">
+                      {/* <button>
                     <i className="fa-solid fa-plus-large" />
                   </button> */}
-                    <button>
-                      <i className="fa-solid fa-gear text-lg" />
-                    </button>
+                      <button>
+                        <i className="fa-solid fa-gear text-lg" />
+                      </button>
+                    </div>
                   </div>
-                </div>
-                <PlaceList
-                  tripId={tripId}
-                  places={places}
-                  setPlaces={(places) => setPlaces(places)}
-                />
-              </div>
-            </section>
-          )}
+                  <PlaceList
+                    tripId={tripId}
+                    places={places}
+                    setPlaces={(places) => setPlaces(places)}
+                  />
+                </article>
+              )}
+            </div>
+          </section>
 
           <Mapbox />
         </div>
