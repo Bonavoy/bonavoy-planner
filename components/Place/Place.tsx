b6e050f8fea67eb928189552b92d0ae19ddbaeee
Shane Goonasekera <theshanukg@gmail.com>
2023-06-01 12:35:25 -0600

    last stuff

--- components/Place/Place.tsx
+++ components/Place/Place.tsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { eachDayOfInterval } from 'date-fns';
+import { eachDayOfInterval, isSameDay } from 'date-fns';
+import clsx from 'clsx';
+
+//components
+import Dayplan from './Dayplan/Dayplan';
 
 //queries
 import { GET_PLACE } from '~/graphql/queries/place';
@@ -11,6 +16,8 @@ interface PlaceProps {
 }
 
 export default function Place({ placeId }: PlaceProps) {
+  const [day, setDay] = useState<null | Date>(null);
+
   const placeDetailsQuery = useQuery(GET_PLACE, {
     variables: { placeId },
   });
@@ -21,23 +28,36 @@ export default function Place({ placeId }: PlaceProps) {
   return (
     <article className="flex h-full">
       <aside className="flex h-full w-max flex-col gap-12 bg-grayPrimary/5 py-32">
-        {eachDayOfInterval({
-          start: new Date(placeDetailsQuery.data?.place.startDate),
-          end: new Date(placeDetailsQuery.data?.place.endDate),
-        }).map((date, index) => (
-          <h2
-            className="flex flex-col px-4 text-lg font-semibold leading-tight"
-            key={date.getTime()}
-          >
-            Day {index + 1}
-            <span className="text-xs font-normal">
-              {formatDate(date, {
-                month: 'short',
-                day: 'numeric',
-              })}
-            </span>
-          </h2>
-        ))}
+        <ul>
+          {eachDayOfInterval({
+            start: new Date(placeDetailsQuery.data?.place.startDate),
+            end: new Date(placeDetailsQuery.data?.place.endDate),
+          }).map((date, index) => (
+            <li
+              key={date.getTime()}
+              className={clsx(
+                isSameDay(day!, date) ? 'bg-primary text-white' : 'text-black',
+              )}
+            >
+              <button
+                type="button"
+                title={`Day ${index + 1}`}
+                className="py-4"
+                onClick={() => setDay(date)}
+              >
+                <h2 className="flex flex-col px-4 text-lg font-semibold leading-tight">
+                  Day {index + 1}
+                  <span className="text-xs font-normal">
+                    {formatDate(date, {
+                      month: 'short',
+                      day: 'numeric',
+                    })}
+                  </span>
+                </h2>
+              </button>
+            </li>
+          ))}
+        </ul>
       </aside>
 
       <div className="px-8 py-8">
@@ -55,6 +75,11 @@ export default function Place({ placeId }: PlaceProps) {
             day: 'numeric',
           })}
         </p>
+        {day && (
+          <section>
+            <Dayplan />
+          </section>
+        )}
       </div>
     </article>
   );
